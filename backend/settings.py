7352460311b2d61716c816e25200078f9a78b979
Hames-1616 <bilquis748@gmail.com>
2023-01-29 11:53:35 +0530

    js

--- backend/settings.py
+++ backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT="C:/Users/shahh/OneDrive/Documents/intern/Essential_django_backend/static"
+STATIC_ROOT="C:/Users/shahh/OneDrive/Documents/intern/Essential_django_backend/assets/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
