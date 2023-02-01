397aee2c36bd78d8109faeece701209a8c537e31
Hames-1616 <bilquis748@gmail.com>
2023-02-01 14:45:06 +0530

    up

--- backendf/urls.py
+++ backendf/urls.py
@@ -5,7 +5,7 @@ from .views import EmailPasswordViewSet
 urlpatterns = [
     path('',views.getlog),
     path('service/',views.getservices),
-    path('allservices',views.getallservices),
+    path('allservices/',views.getallservices),
     path('popular/',views.getpop),
     path('insert/',EmailPasswordViewSet.as_view({'post':'insert'}))
 ]
