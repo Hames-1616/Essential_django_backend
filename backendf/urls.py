d753c19de94e040ac5699155052128162932cdab
Hames-1616 <bilquis748@gmail.com>
2023-02-03 00:28:31 +0530

    cleaning

--- backendf/urls.py
+++ backendf/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import EmailPasswordViewSet
+from .views import *
 
 urlpatterns = [
     path('',views.getlog),
     path('service/',views.getservices),
     path('allservices/',views.getallservices),
     path('popular/',views.getpop),
-    path('cleaning/',views.cleaning),
-    path('insert/',EmailPasswordViewSet.as_view({'post':'insert'}))
+    path('clean/',views.cleaning),
+    path('insert/',EmailPasswordViewSet.as_view({'post':'insert'})),
+    path('cleaning/',serviceviewset.as_view({'post':'ins'}))
 ]
